bb5437e34f8b4fe3fb2051efee0a30cdceb90e67
Nidhin Joseph Nelson <nidhinjnel@gmail.com>
2026-01-24 22:38:03 -0330

    Create schema and roles for 'DataSecured' database
    Added schema and roles for 'DataSecured' database.

--- scripts/create_schemas.sql
+++ scripts/create_schemas.sql
@@ -4,17 +4,24 @@ Create DataSecured Database
 -------------------------------------------------------------
 Script Purpose:
     Creates the 'DataSecured' database
+    Create schema for 'DataSecured'
+    Create roles for 'DataSecured'
 
 Architecture Overview:
     raw        - Source landing tables (ingested as is)
     core       - Cleaned and standardized enterprise data
     analytics  - BI ready fact and dimension models
-    semantic   - Business friendly consumption views
     security   - Role and access control metadata
-    audit      - Load tracking and governance logs
+
+Role Overview:
+    db_partner           
+    db_manager           
+    db_senior_consultant
+    db_junior_consultant 
+    db_finance           
+    db_client_portal      
 
 WARNING:
-
     DROPS existing 'DataSecured' database and ALL data.
     Ensure backups exist before running this script.
 */
@@ -47,5 +54,15 @@ CREATE SCHEMA analytics;
 GO
 
 CREATE SCHEMA security;
+GO
+
+-- Create roles
+CREATE ROLE db_partner;
+CREATE ROLE db_manager;
+CREATE ROLE db_senior_consultant;
+CREATE ROLE db_junior_consultant;
+CREATE ROLE db_finance;
+CREATE ROLE db_client_portal;
+
 GO
 
